14d8e0617f774dbc5214f9c5293093bd87a6244a
Haerden <719042341@qq.com>
2020-01-27 12:45:46 +0800

    add useMemo useCallback

--- src/modules/Hooks/four.jsx
+++ src/modules/Hooks/four.jsx
@@ -1,17 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo, useCallback, memo } from 'react';
+
+const Counter = memo(function Counter(props) {
+    console.log('Counter render');
+
+    return <h1 onClick={props.onClick}>{props.count}</h1>
+});
 
 export default function App() {
-
     const [count, setCount] = useState(0);
+
+    const double = useMemo(() => {
+        return count * 2;
+    }, [count === 4]);
+
+    // half 依赖 double
+    // const half = useMemo(() => { return double / 4; }, [double]); ...
+    const onClick = useMemo(() => {
+        return () => {
+            console.log('click!');
+        }
+    }, []);
+
+    /*
+    const onClick = useCallback(() => {
+        console.log('click!');
+    }, []);
+    */
+
+    // useMemo(() => fn) , useCallBack(fn)
 
     return (
         <div>
             <button type="button"
                 onClick={() => setCount(count => count + 1)}
             >
-                Click ({count})
-
-        </button>
+                Click: ({count}) double: ({double})
+            </button>
+            <Counter count={double} onClick={onClick} />
         </div>
     )
 }
